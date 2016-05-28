eb40df6a0a54fca1bf1e1209f072e13d5d078041
YY <yingy4@hotmail.com>
2016-05-28 20:31:22 +0800

    Finish Getting Up and Running, Data Types courses.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,97 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*
+var formattedName = HTMLheaderName.replace("%data%","YY");
+var formattedRole = HTMLheaderRole.replace("%data%","Engineer");
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+  */
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "A",
+			"title" : "B",
+			"location" : "C",
+			"dates" : "D",
+			"description" : "E"
+		},
+		{
+			"employer" : "A",
+			"title" : "B",
+			"location" : "C",
+			"dates" : "D",
+			"description" : "E"
+		}
+	]
+};
+
+var project = {
+	"projects" : [
+		{
+			"title" : "A",
+			"dates" : "B",
+			"description" : "C",
+			"images" : [
+				"A","B"
+			]
+		}
+	]
+};
+
+var bio = {
+	"name" : "YY",
+	"role" : "Engineer",
+	"contacts" : {
+		"mobile" : "1",
+		"email" : "2",
+		"github" : "3",
+		"location" : "4"
+	},
+	"welcomeMessage" : "hello",
+	"skill" : [
+		"A","B"
+	]
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "A",
+			"location" : "B",
+			"degree" : "C",
+			"majors" : [
+				"A","B"
+			],
+			"dates" : "1",
+			"url" : "2"
+		},
+		{
+			"name" : "A",
+			"location" : "B",
+			"degree" : "C",
+			"majors" : [
+				"A","B"
+			],
+			"dates" : "1",
+			"url" : "2"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "A",
+			"school" : "B",
+			"dates" : "C",
+			"url" : "D"
+		},
+		{
+			"title" : "A",
+			"school" : "B",
+			"dates" : "C",
+			"url" : "D"			
+		}
+	]
+};
