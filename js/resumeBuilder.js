c722936998625aecd8b9cb299ce0641d04e0dd60
YY <yingy4@hotmail.com>
2016-05-31 20:56:24 +0800

    Finish the project v1.0

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,29 +2,22 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-/*
-var formattedName = HTMLheaderName.replace("%data%","YY");
-var formattedRole = HTMLheaderRole.replace("%data%","Engineer");
-
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
-  */
 
 var work = {
 	"jobs" : [
 		{
-			"employer" : "A",
-			"title" : "B",
-			"location" : "C",
-			"dates" : "D",
-			"description" : "E"
+			"employer" : "Wolong National Nature Reserve",
+			"title" : "Actor",
+			"location" : "Sichuan",
+			"dates" : "2010-2013",
+			"description" : "China Conservation and Research Center for the Giant Panda at the Wolong National Nature Reserve, Sichuan."
 		},
 		{
-			"employer" : "A",
-			"title" : "B",
-			"location" : "C",
-			"dates" : "D",
-			"description" : "E"
+			"employer" : "London Zoo",
+			"title" : "Actor",
+			"location" : "London",
+			"dates" : "2013-2016",
+			"description" : "Chi Chi at the London Zoo became very popular. This influenced the World Wildlife Fund to use a panda as its symbol."
 		}
 	]
 };
@@ -32,66 +25,166 @@ var work = {
 var project = {
 	"projects" : [
 		{
-			"title" : "A",
-			"dates" : "B",
-			"description" : "C",
+			"title" : "Animal Trading Cards",
+			"dates" : "2016-2016",
+			"description" : "project for Panda in Udacity",
 			"images" : [
-				"A","B"
+				"images/card.png"
+			]
+		},
+		{
+			"title" : "My Portfolio",
+			"dates" : "2016-2016",
+			"description" : "project for Panda in Udacity",
+			"images" : [
+				"images/myPortfolio1.png","images/myPortfolio2.png","images/myPortfolio3.png"
 			]
 		}
 	]
 };
 
 var bio = {
-	"name" : "YY",
-	"role" : "Engineer",
+	"name" : "Great Panda",
+	"role" : "Master",
 	"contacts" : {
-		"mobile" : "1",
-		"email" : "2",
-		"github" : "3",
-		"location" : "4"
+		"mobile" : "1234567890",
+		"email" : "myemail@email.com",
+		"github" : "mygithub",
+		"location" : "Shanghai"
 	},
-	"welcomeMessage" : "hello",
+	"welcomeMessage" : "We like eating bamboo!",
 	"skill" : [
-		"A","B"
-	]
+		"Eat","Sleep","Play","Climb","Swim"
+	],
+	"biopic" : "images/panda1.jpg"
 };
 
 var education = {
 	"schools" : [
 		{
-			"name" : "A",
-			"location" : "B",
-			"degree" : "C",
+			"name" : "Peking University",
+			"location" : "Beijing",
+			"degree" : "Bachelor",
 			"majors" : [
-				"A","B"
+				"Eating","Sleeping"
 			],
-			"dates" : "1",
-			"url" : "2"
+			"dates" : "2000-2004",
+			"url" : "http://www.pku.edu.cn/"
 		},
 		{
-			"name" : "A",
-			"location" : "B",
-			"degree" : "C",
+			"name" : "Fudan University",
+			"location" : "Shanghai",
+			"degree" : "Master",
 			"majors" : [
-				"A","B"
+				"Swimming","Climbing"
 			],
-			"dates" : "1",
-			"url" : "2"
+			"dates" : "2004-2006",
+			"url" : "http://www.fudan.edu.cn/index.html"
 		}
 	],
 	"onlineCourses" : [
 		{
-			"title" : "A",
-			"school" : "B",
-			"dates" : "C",
-			"url" : "D"
+			"title" : "Front-End Web Developer Nanodegree",
+			"school" : "Udacity",
+			"dates" : "2006-2007",
+			"url" : "https://cn.udacity.com/"
 		},
 		{
-			"title" : "A",
-			"school" : "B",
-			"dates" : "C",
-			"url" : "D"			
+			"title" : "Playing",
+			"school" : "Coursera",
+			"dates" : "2007-2008",
+			"url" : "https://www.coursera.org/"			
 		}
 	]
 };
+
+
+bio.display = function() {
+
+
+	$("#header").append(HTMLheaderName.replace("%data%",bio.name));
+	$("#header").append(HTMLheaderRole.replace("%data%",bio.role));
+	$("#header").append(HTMLbioPic.replace("%data%",bio.biopic));
+	$("#header").find('img').attr('alt','My bio picture');
+	$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage));
+	$("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+	$("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	$("#footerContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+	$("#footerContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+	if (bio.skill.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		for (var i in bio.skill) {
+			$("#skills").append(HTMLskills.replace("%data%",bio.skill[i]));
+		}
+	}
+};
+
+bio.display();
+
+work.display = function() {
+	if (work.jobs.length > 0) {
+		for (var i in work.jobs) {
+			$("#workExperience").append(HTMLworkStart);
+			$(".work-entry:last").append(HTMLworkEmployer.replace("%data%",work.jobs[i].employer)+HTMLworkTitle.replace("%data%",work.jobs[i].title));
+			$(".work-entry:last").append(HTMLworkDates.replace("%data%",work.jobs[i].dates));
+			$(".work-entry:last").append(HTMLworkLocation.replace("%data%",work.jobs[i].location));
+			$(".work-entry:last").append(HTMLworkDescription.replace("%data%",work.jobs[i].description));
+		}
+	}	
+};
+
+work.display();
+
+$(document).click(function(loc) { 
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x,y);
+});
+
+$("#main").append(internationalizeButton);
+
+function inName(name) {
+	var names = name.split(" ");
+	return names[0].slice(0,1).toUpperCase()+names[0].slice(1).toLowerCase()+" "+names[1].toUpperCase();
+};
+
+project.display = function() {
+	if (project.projects.length > 0) {
+		for (var i in project.projects) {
+			$("#projects").append(HTMLprojectStart);
+			$(".project-entry:last").append(HTMLprojectTitle.replace("%data%",project.projects[i].title));
+			$(".project-entry:last").append(HTMLprojectDates.replace("%data%",project.projects[i].dates));
+			$(".project-entry:last").append(HTMLprojectDescription.replace("%data%",project.projects[i].description));
+			if (project.projects[i].images.length > 0) {
+				for (var j in project.projects[i].images) {
+					$(".project-entry:last").append(HTMLprojectImage.replace("%data%",project.projects[i].images[j]));
+				}
+				$(".project-entry:last").find('img').attr('alt','Picture for my project');
+			}
+		}
+	}
+};
+
+project.display();
+
+education.display = function() {
+	if (education.schools.length > 0) {
+		for (var i in education.schools) {
+			$("#education").append(HTMLschoolStart);
+			$(".education-entry:last").append(HTMLschoolName.replace("%data%",education.schools[i].name)+HTMLschoolDegree.replace("%data%",education.schools[i].degree));
+			$(".education-entry:last").find('a').attr('href',education.schools[i].url);
+			$(".education-entry:last").append(HTMLschoolDates.replace("%data%",education.schools[i].dates));
+			$(".education-entry:last").append(HTMLschoolLocation.replace("%data%",education.schools[i].location));
+			if (education.schools[i].majors.length > 0) {
+				for (var j in education.schools[i].majors) {
+					$(".education-entry:last").append(HTMLschoolMajor.replace("%data%",education.schools[i].majors[j]));
+				}
+			}
+		}
+	}
+};
+
+education.display();
+
+$("#mapDiv").append(googleMap);
